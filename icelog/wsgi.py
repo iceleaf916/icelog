cd0afb686268dd3ff09cf30a6e2d61efc041006b
iceleaf916 <kaisheng.ye@gmail.com>
2013-09-11 00:55:42 +0800

    update configuration

--- icelog/wsgi.py
+++ icelog/wsgi.py
@@ -1,15 +1,6 @@
 # coding: utf-8
 
-import sys, os
-
-abspath = os.path.abspath(__file__)
-dirname = os.path.dirname(abspath)
-updir = os.path.dirname(dirname)
-
-if updir not in sys.path:
-    sys.path.insert(0, updir)
-if dirname not in sys.path:
-    sys.path.insert(0, dirname)
+import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "icelog.settings")
 
